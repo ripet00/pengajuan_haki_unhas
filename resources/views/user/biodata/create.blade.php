2e166893283523622b7c76cd8ec5355afcc36d3a
mknayam16 <zdenzel7@gmail.com>
2025-11-11 17:22:03 +0800

    edited biodatas warning messaage

--- resources/views/user/biodata/create.blade.php
+++ resources/views/user/biodata/create.blade.php
@@ -242,7 +242,7 @@
                     <div class="bg-blue-50 border border-blue-200 rounded-lg p-3 mt-2">
                         <p class="text-sm text-blue-800">
                             <i class="fas fa-info-circle mr-1"></i>
-                            <strong>Field Wajib Diisi:</strong> Nama Lengkap (*), NIK (*), Pekerjaan (*), Alamat (*), Email (*), dan Nomor HP (*)
+                            <strong>Field Wajib Diisi:</strong> Semua field wajib diisi dan pastikan data sudah benar dan lengkap sebelum submit.
                         </p>
                     </div>
                 </div>
