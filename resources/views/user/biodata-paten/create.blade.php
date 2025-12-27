e3f6915269a4aa3e7682719c4b7a4e81811d4598
mknayam16 <zdenzel7@gmail.com>
2025-12-27 17:25:03 +0800

    changed biodata paten 'Pekerjaan' field example note

--- resources/views/user/biodata-paten/create.blade.php
+++ resources/views/user/biodata-paten/create.blade.php
@@ -381,7 +381,7 @@
                                    class="w-full px-3 py-2 border ${!!inventor.error_pekerjaan ? 'border-red-300 bg-red-50' : 'border-gray-300'} rounded-lg focus:ring-2 focus:ring-green-500 focus:border-transparent"
                                    required>
                             <p class="text-xs text-gray-500 mt-1">
-                                <i class="fas fa-info-circle mr-1"></i>Khusus Dosen, isi dengan lengkap. Contoh: "Dosen Fakultas Teknik Universitas Hasanuddin"
+                                <i class="fas fa-info-circle mr-1"></i>Contoh: "Dosen Fakultas Teknik Universitas Hasanuddin"
                             </p>
                         </div>
                         
